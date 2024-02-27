ed4afd2d38bdc9e09bd4cf5c92b4c08bffbe81b0
Rakib Hasan Babu <rhbabu3@gmail.com>
2024-02-28 00:08:15 +0600

    init :tada: react blogify initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <h1 className="text-5xl font-extrabold text-center text-violet-500">
+                REACT BLOGIFY
+            </h1>
+        </div>
+    );
+}
+
+export default App;
